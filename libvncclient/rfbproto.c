3c9363381406f8d7400c963a5c274a9b510edf60
Everton Vieira <everton.vieira@venturus.org.br>
2021-08-09 10:54:39 -0300

    test capslock

--- libvncclient/rfbproto.c
+++ libvncclient/rfbproto.c
@@ -1588,11 +1588,11 @@ SendKeyEvent(rfbClient* client, uint32_t key, rfbBool down)
   ke.down = down ? 1 : 0;
   ke.key = rfbClientSwap32IfLE(key);
   if (key == XK_Caps_Lock) {
-      //      rfbClientLog("A tecla caps lock foi press \n");
-      //      rfbClientLog("valor do estado: %d\n", down);
-      //      rfbClientLog("valor do pad: %d\n", rfbClientSwap16IfLE((down& 1) != 0));
+        rfbClientLog("A tecla caps lock foi press \n");
+        rfbClientLog("valor do estado: %d\n", down);
+        rfbClientLog("valor do pad: %d\n", rfbClientSwap16IfLE((down& 1) != 0));
       //      ke.pad = rfbClientSwap16IfLE( (GetKeyState(VK_CAPITAL) & 1) != 0);
-      ke.pad = rfbClientSwap16IfLE((down& 1) != 0);
+        ke.pad = rfbClientSwap16IfLE((down& 1) != 0);
   }
   return WriteToRFBServer(client, (char *)&ke, sz_rfbKeyEventMsg);
 }
